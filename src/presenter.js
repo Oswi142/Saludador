e47e18fb5526bd7f0895128e15efd7e3342d6dda
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-02-22 23:05:05 -0400

    Saludando por idioma

--- src/presenter.js
+++ src/presenter.js
@@ -2,10 +2,12 @@ import verificarGenero from "./verificarDatos";
 
 const nombre = document.querySelector("#nombre");
 const genre = document.querySelector("#genre");
+const edad = document.querySelector("#edad");
+const leng = document.querySelector("#leng");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#saludo-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  div.innerHTML = "<p>" + verificarGenero(genre.value,nombre.value) + "</p>";
+  div.innerHTML = "<p>" + verificarGenero(genre.value,nombre.value,edad.value,leng.value) + "</p>";
 });
--- src/verificarDatos.js
+++ src/verificarDatos.js
@@ -1,12 +1,43 @@
-function verificarGenero(genre,nombre)
+function verificarGenero(genre,nombre,edad,leng)
 {
     var saludo=" ";
-    if(genre=='M'){
-        saludo="Hola señor "+nombre;
+    if(leng=="ES"){
+        if(genre=='M'){
+            if(edad>=30){
+                saludo="Hola señor "+nombre;
+            }
+            else{
+                saludo="Hola joven "+nombre;
+            }
+        }
+        else{
+            if(edad>=30){
+                saludo="Hola señora "+nombre;
+            }
+            else{
+                saludo="Hola señorita "+nombre;
+            }
+        }
     }
-    else{
-        saludo="Hola señora "+nombre;
+    if(leng=="EN"){
+        if(genre=='M'){
+            if(edad>=30){
+                saludo="Hello Mr. "+nombre;
+            }
+            else{
+                saludo="Hello  "+nombre;
+            }
+        }
+        else{
+            if(edad>=30){
+                saludo="Hello Mrs. "+nombre;
+            }
+            else{
+                saludo="Hola Ms. "+nombre;
+            }
+        }
     }
+    
     return saludo;
 }
 
